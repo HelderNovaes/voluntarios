e51af6dfe9d6895d20d30a609c95012baf752d0f
Helder Novaes <heldepes@gmail.com>
2025-05-22 19:34:50 -0400

    s

---
+++ netlify/functions/verificarSenha.js
@@ -0,0 +1,17 @@
+exports.handler = async (event) => {
+  const { senha } = JSON.parse(event.body);
+
+  const senhaCorreta = process.env.SENHA_ACESSO;
+
+  if (senha === senhaCorreta) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ acesso: true }),
+    };
+  } else {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ acesso: false }),
+    };
+  }
+};
